b03d0b9bf5ac8dadb2346a6f766693971f155cbd
Amurkibon <tmalanov@inbox.ru>
2024-02-14 03:05:12 +0800

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://438aca5c-19e6-4a97-8a7d-2dafb6fcf060.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK = "/api/v1/orders/track"
+
+
+
+
---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order = {
+    "firstName": "Тимур",
+    "lastName": "Маланов",
+    "address": "Пушкина, дом Колотушкина",
+    "metroStation": 10,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-23",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
