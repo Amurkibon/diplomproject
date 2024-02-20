527cf8e217f39503d8c78ae94e10c21c41937d97
Amurkibon <154439220+Amurkibon@users.noreply.github.com>
2024-02-21 02:22:25 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
-URL_SERVICE = "https://438aca5c-19e6-4a97-8a7d-2dafb6fcf060.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://00241afb-fef6-4b34-ba0e-c586a18aedb1.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"  
 CREATE_ORDER_PATH = "/api/v1/orders"
-GET_ORDER_BY_TRACK = "/api/v1/orders/track"
+GET_ORDER_BY_TRACK = "/api/v1/orders/track?t="
 
 
 
